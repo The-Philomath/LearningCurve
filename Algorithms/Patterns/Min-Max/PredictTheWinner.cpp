8e5aea0fd660b8079f490b4e3f4ef234da7fd814
ThePhilomath <punia.ramandeep@gmail.com>
2019-12-16 13:55:18 +0530

    Update PredictTheWinner.cpp

--- Algorithms/Patterns/Min-Max/PredictTheWinner.cpp
+++ Algorithms/Patterns/Min-Max/PredictTheWinner.cpp
@@ -42,7 +42,7 @@ bool PredictTheWinner(vector<int>& nums) {
         return CheckWinner(nums, 0, nums.size() - 1 ) >= 0;
     }
 
-// Recursive memoization / DP Top Down 
+// Recursive memoization / DP Top Down
 
 int CheckWinner(vector<vector<int>>& mem, vector<int>& nums, int start, int end){
         if(start == end) return nums[start];
@@ -60,7 +60,29 @@ bool PredictTheWinner(vector<int>& nums) {
     }
 
 // DP Bottom up Solution
-// Smallest subproblem can be considered as there is only one element in array so the score of player 1 will be equal to that // element.
-// we can create bigger subproblem with the solution of this problem. considering the 2 elements(interval) at a time then 3 // and so on upto n.
-// For every such interval if we choose the the first element of interval then opponent must have chosen the 
-// https://algorithms.tutorialhorizon.com/dynamic-programming-coin-in-a-line-game-problem/
+// Smallest subproblem can be considered as there is only one element in array so the score of player 1 will be equal to that element.
+// we can create bigger subproblem with the solution of this subproblem. considering the 2 elements(interval) at a time then 3 and so on upto n.
+// let's consider subproblem with 2 elements at a time.
+// We choose first one then the second person will choose another.
+// if there are three elements and we choose first element then for the player 2 subproblem is a set of 2 and we already have a solution for that.
+// so subtracting the choosen element from the solution of player2's subproblem will give us the solution.
+
+//Further Reference: https://algorithms.tutorialhorizon.com/dynamic-programming-coin-in-a-line-game-problem/
+
+bool PredictTheWinner(vector<int>& nums) {
+        vector<vector<int>> mem (nums.size(),vector<int>(nums.size(), INT_MIN));
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            mem[i][i] = nums[i];
+        }
+        for(int interval = 2; interval <= nums.size(); interval++)
+        {
+            for(int i = 0; j = interval - 1, j < nums.size(); i++, j++)
+            {
+                mem[i][j] = max(nums[i] - mem[i+1][j], nums[j] - mem[i][j-1]);
+            }
+        }
+
+        return mem[0][nums.size()-1] >= 0;
+    }
