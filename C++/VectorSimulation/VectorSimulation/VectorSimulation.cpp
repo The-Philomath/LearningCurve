d8a9c7834168699cef61b8cc57a1d504805cf526
The-Philomath <punia.ramandeep@gmail.com>
2019-12-16 20:51:49 +0530

    STL vector implementation

---
+++ C++/VectorSimulation/VectorSimulation/VectorSimulation.cpp
@@ -0,0 +1,13 @@
+// VectorSimulation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "VectorSimulation.h"
+
+int main()
+{
+    vec<int> v;
+    v.push(10);
+    std::cout << v[0];
+}
+
---
+++ C++/VectorSimulation/VectorSimulation/VectorSimulation.h
@@ -0,0 +1,36 @@
+#pragma once
+
+template <typename T>
+class vec
+{
+	T* arr{ nullptr };
+	size_t sz{ 0 };
+
+public:
+	vec();
+	void push(int data);
+	T operator[](int index)
+	{
+		return *(arr + index);
+	}
+};
+
+template<typename T>
+inline vec<T>::vec()
+{
+    
+}
+
+template<typename T>
+inline void vec<T>::push(int data)
+{
+	++sz;
+	T* temp = new T(sz);
+	for (unsigned int i = 0; i < sz - 1; i++)
+	{
+		*(temp + i) = *(arr + i);
+	}
+	*(temp + sz - 1) = data;
+	delete arr;
+	arr = temp;
+}
